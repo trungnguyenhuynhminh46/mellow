6397d97bded7eaabd23409182972ce91e48c73c7
Nguyen Huynh Minh Trung <trungnguyenhuynhminh46@gmail.com>
2024-04-21 21:38:23 +0700

    Frontend: Config file alias

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { ThemeContextProvider } from './shared/ThemeContext.tsx'
+import { ThemeContextProvider } from '@shared/ThemeContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
