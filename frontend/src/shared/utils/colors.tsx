b8b598d889c34a4a5051d6e97dfd26c6dbaacf69
Nguyen Huynh Minh Trung <trungnguyenhuynhminh46@gmail.com>
2024-04-27 14:37:14 +0700

    BoardDetail_refactor: refactor colors.tsx

--- frontend/src/shared/utils/colors.tsx
+++ frontend/src/shared/utils/colors.tsx
@@ -13,27 +13,27 @@ export const generateImageElement = (imageUrl: string): HTMLImageElement => {
 }
 
 export const generateAverageColor = (imageUrl: string): RGB => {
-  const imgElement = generateImageElement(imageUrl)
-  const blockSize = 5 // only visit every 5 pixels
-  const defaultRGB: RGB = new RGB(0, 0, 0)
   const canvas = document.createElement('canvas')
-  const context = canvas.getContext && canvas.getContext('2d')
+  const ctx = canvas.getContext && canvas.getContext('2d')
+  const imgElement = generateImageElement(imageUrl)
+  const blockSize = 5
+  const defaultRGB: RGB = new RGB(0, 0, 0)
   let data: ImageData | null
   let i = -4
   const rgb: RGB = new RGB(0, 0, 0)
   let count = 0
 
-  if (!context) {
+  if (!ctx) {
     return defaultRGB
   }
 
   const height = canvas.height = imgElement.naturalHeight || imgElement.offsetHeight || imgElement.height
   const width = canvas.width = imgElement.naturalWidth || imgElement.offsetWidth || imgElement.width
 
-  context.drawImage(imgElement, 0, 0)
+  ctx.drawImage(imgElement, 0, 0)
 
   try {
-    data = context.getImageData(0, 0, width, height)
+    data = ctx.getImageData(0, 0, width, height)
   } catch (e) {
     return defaultRGB
   }
@@ -47,6 +47,7 @@ export const generateAverageColor = (imageUrl: string): RGB => {
     rgb.b += data.data[i + 2]
   }
 
+  // Operator ~~ just chops off the decimal -1.2 -> -1, 1.2 -> 1
   rgb.r = ~~(rgb.r / count)
   rgb.g = ~~(rgb.g / count)
   rgb.b = ~~(rgb.b / count)
@@ -60,7 +61,7 @@ export const generateColorByAlpha = (base: RGB, alpha: number): RGB => {
 
 export const generateDifferentShadeOfColor = (color: RGB, numberOfShades: number): RGB[] => {
   const unitColor = new RGB(Math.floor(color.r / 10), Math.floor(color.g / 10), Math.floor(color.b / 10))
-  const iStart = -Math.floor((numberOfShades-1)/2)
+  const iStart = - Math.floor((numberOfShades-1)/2)
   const iEnd = Math.floor(numberOfShades/2)
   const shades = []
   for (let i = iStart; i <= iEnd; i++) {
@@ -71,12 +72,6 @@ export const generateDifferentShadeOfColor = (color: RGB, numberOfShades: number
 }
 
 export const generateThemeColors = (themeColor: RGB | string): RGB[] => {
-  let color: RGB
-  if (typeof themeColor === 'string') {
-    color = generateAverageColor(themeColor)
-  }
-  else {
-    color = themeColor
-  }
+  const color: RGB = typeof themeColor === 'string' ? generateAverageColor(themeColor) : themeColor
   return generateDifferentShadeOfColor(color, 10)
 }
