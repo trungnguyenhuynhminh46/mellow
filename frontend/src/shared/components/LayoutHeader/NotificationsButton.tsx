805ce5bfcedf79f47091715ec79bb768bad43eb7
Nguyen Huynh Minh Trung <trungnguyenhuynhminh46@gmail.com>
2024-04-29 15:45:48 +0700

    BoardDetail_AppHeader: Implement Notification Button

---
+++ frontend/src/shared/components/LayoutHeader/NotificationsButton.tsx
@@ -0,0 +1,20 @@
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import { IconButton, useTheme } from '@mui/material'
+import { CustomThemeType } from '@shared/types/theme.ts'
+import { alpha } from '@mui/material/styles'
+const NotificationsButton = () => {
+  const theme = useTheme<CustomThemeType>()
+  return (
+    <IconButton aria-label="delete" size="medium" sx={{
+      marginLeft: theme.spacing(1),
+      color: theme.palette.common.white,
+      '&:hover': {
+        background: alpha(theme.palette.common.white, 0.1)
+      }
+    }}>
+      <NotificationsIcon fontSize="inherit" />
+    </IconButton>
+  )
+}
+
+export default NotificationsButton
