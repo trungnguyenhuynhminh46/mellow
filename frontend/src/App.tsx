a308e8d70ed5f02c94307e0eef84e2a663bdb9ab
Nguyen Huynh Minh Trung <trungnguyenhuynhminh46@gmail.com>
2024-05-06 08:49:09 +0700

    BoardDetail_AppHeader: Disable text selection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from '@mui/material'
+import { GlobalStyles, ThemeProvider } from '@mui/material'
 import { useThemeContext } from '@shared/ThemeContext.tsx'
 import AppRoutes from '@/AppRoutes.tsx'
 
@@ -9,6 +9,13 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline/>
+      <GlobalStyles styles={{
+        body: {
+          '-webkit-user-select': 'none', /* Safari */
+          '-ms-user-select': 'none', /* IE 10 and IE 11 */
+          'user-select': 'none' /* Standard syntax */
+        }
+      }} />
       <AppRoutes />
     </ThemeProvider>
   )
